aa35a60145efe05033bbc89ea56b0d076d7f878d
Shahid Hasan <96911957+grazierShahid@users.noreply.github.com>
2024-09-17 15:43:49 +0600

    Time: 0 ms (100%), Space: 8.3 MB (19.8%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        
+        long long lo = 0, hi = x, mid;
+        int ret;
+        while(lo<=hi){
+            mid = (lo+hi)>>1;
+            if(mid*mid==x) return mid;
+            if(mid*mid<x) {
+                lo = mid+1;
+                ret = (int)mid;
+            } 
+            else hi = mid-1;
+        }
+        return ret;
     }
 };
