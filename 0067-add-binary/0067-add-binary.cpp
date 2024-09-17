78a248119a1cf07f19d3177ab82d3096c5a1aaf6
Shahid Hasan <96911957+grazierShahid@users.noreply.github.com>
2024-09-17 15:40:01 +0600

    Time: 3 ms (54.15%), Space: 10.2 MB (33.63%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        int i=a.size()-1, j=b.size()-1, carr=0;
+        string s="";
+
+        while(i>=0 || j>=0){
+            int x = (i>=0)? (a[i--]-48):0;
+            int y = (j>=0)? (b[j--]-48):0;
+            int sum = x+y+carr;
+            carr = (sum>1)? 1:0;
+            sum = (sum>1)? sum-2:sum;
+            s += to_string(sum);
+        }
+        if(carr) s+='1';
+        reverse(s.begin(), s.end());
+        return s;
+    }
+};
