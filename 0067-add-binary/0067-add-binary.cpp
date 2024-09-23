1e4179019f495585046ead75598ef3cdf0bc048f
Shahid Hasan <96911957+grazierShahid@users.noreply.github.com>
2024-09-23 16:44:16 +0600

    Time: 2 ms (62.11%), Space: 8.3 MB (76.24%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -4,18 +4,15 @@ public:
         int i=a.size()-1, j=b.size()-1, car=0;
         string s="";
 
-        while(i>=0 || j>=0){
+        while(i>=0 || j>=0 || car){
             int x = (i>=0)? a[i--]-48:0;
             int y = (j>=0)? b[j--]-48:0;
-            int sum = x+y+car;
+            car += x+y;
 
-            car = (sum>1)?1:0;
-            sum = (sum>1)?sum-2:sum;
-
-            s += to_string(sum);
+            s += car % 2 + '0';
+            car /= 2;
         }
 
-        if(car) s+='1';
         reverse(s.begin(), s.end());
         return s;
     }
