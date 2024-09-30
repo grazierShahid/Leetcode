7c84a99eebfbb9842831b83da14b3303f1391d2a
Shahid Hasan <96911957+grazierShahid@users.noreply.github.com>
2024-09-30 13:55:26 +0600

    Time: 484 ms (75.01%), Space: 0B (100%) - LeetHub

--- 0183-customers-who-never-order/0183-customers-who-never-order.sql
+++ 0183-customers-who-never-order/0183-customers-who-never-order.sql
@@ -1,4 +1,4 @@
 select c.name as Customers
 from customers c left join orders o
 on c.id = o.customerID
-where (select count(customerID) from orders where c.id = customerID) = 0;
+where o.customerID is null;
