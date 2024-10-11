ff2f5411f06d7be3c37a81acd192b2f50a387a4d
Shahid Hasan <96911957+grazierShahid@users.noreply.github.com>
2024-10-11 21:51:20 +0600

    Time: 822 ms (25.23%), Space: 0B (100%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -1,4 +1,4 @@
-select d.name as Department, e.name as Employee, Salary 
+select d.name as Department, e.name as Employee, Salary
 from employee e join department d
-on e.departmentID = d.id
-where salary = (select max(salary) from employee where departmentID = d.id);
+on e.departmentId = d.id
+where salary = (select max(salary) from employee where departmentId = d.id);
