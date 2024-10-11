e6a86c46ef8c83245111ece87889459d729209c8
Shahid Hasan <96911957+grazierShahid@users.noreply.github.com>
2024-10-11 20:08:30 +0600

    Time: 318 ms (84.88%), Space: 0B (100%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,4 +1,3 @@
-select distinct p1.email
-from person p1 join person p2
-on p1.email = p2.email
-where p1.id <> p2.id;
+select p1.email from person p1 join person p2
+where p1.email = p2.email and p1.id < p2.id
+group by email;
